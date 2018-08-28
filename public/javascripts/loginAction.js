1c1126f6a3d7e40ac2724da6b8c875274c08f706
hhk998402 <hemant.hari.kumar@gmail.com>
2018-08-28 14:38:20 +0530

    Added profile modal

--- public/javascripts/loginAction.js
+++ public/javascripts/loginAction.js
@@ -1,3 +1,8 @@
 document.querySelector('.img__btn').addEventListener('click', function() {
     document.querySelector('.cont').classList.toggle('s--signup');
 });
+
+let params = (new URL(document.location)).searchParams;
+let name = params.get("register");
+if(name=="true")
+    document.querySelector('.cont').classList.toggle('s--signup');
